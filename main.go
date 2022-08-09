b17b622cd65c00eb64ddb144fc289bca119b4027
santiparp.m <santiparp.m@gksoftware.tech>
2022-08-09 16:06:49 +0700

    setup product

--- main.go
+++ main.go
@@ -1,55 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/i3ankjs/chat/pkg/common/db"
+	"github.com/i3ankjs/chat/pkg/users"
+	"github.com/spf13/viper"
 )
 
-func createListener() (l net.Listener, close func()) {
-	l, err := net.Listen("tcp", ":2222")
-	if err != nil {
-		panic(err)
-	}
-	return l, func() {
-		_ = l.Close()
-	}
-}
-
-type Book struct {
-	ID     string `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-}
-type Config struct {
-	Host     string
-	Port     string
-	Password string
-	User     string
-	DBName   string
-	SSLMode  string
-}
-
-var books = []Book{
-	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
-	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
-	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
-}
-
-func OrderIdHandler(c *gin.Context) {
-	id := c.Param("id")
-	fmt.Printf(id)
-	c.JSON(http.StatusOK, books)
-}
-
 func main() {
-	// db, err = gorm.Open("postgres", "host=gksoftware02.cgpbwmgdgyz2.ap-southeast-1.rds.amazonaws.com port=5432 user=root dbname=gk_dev_db sslmode=disable password=55k3fhGgsbKRMTvR")
-	// if err != nil {
-	// 	panic("failed to connect database")
-	// }
-	r := gin.New()
-	r.GET("/order/:id", OrderIdHandler)
-	http.ListenAndServe(":2222", r)
+	viper.SetConfigFile("./pkg/common/envs/.env")
+	viper.ReadInConfig()
+
+	port := viper.Get("PORT").(string)
+	dbUrl := viper.Get("DB_URL").(string)
+
+	r := gin.Default()
+	h := db.Init(dbUrl)
+
+	users.RegisterRoutes(r, h)
+	// register more routes here
+
+	r.Run(port)
 }
---
+++ pkg/common/models/user.go
@@ -0,0 +1,20 @@
+package models
+
+import "gorm.io/gorm"
+
+type Books struct {
+	gorm.Model
+	UserId             uint    `gorm:"primary key;autoIncrement" json:"user_id"`
+	Name               *string `json:"name"`
+	Email              *string `json:"email"`
+	Password           *string `json:"password"`
+	Pic                *string `json:"pic"`
+	Address            *string `json:"address"`
+	Telephone          *string `json:"telephone"`
+	Status             *string `json:"status"`
+	ShopId             *string `json:"shop_id"`
+	GroupId            *string `json:"group_id"`
+	TokenResetPassword *string `json:"token_reset_password"`
+	DateAdded          *string `json:"date_added"`
+	DateModified       *string `json:"date_modified"`
+}
